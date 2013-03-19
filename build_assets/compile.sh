8ef75818b1c8ead0f9895f050dcbb10127fc39f1
Goszu <marcingosz@gmail.com>
2013-03-19 17:20:56 +0000

    Adding comments to the build script

--- build_assets/compile.sh
+++ build_assets/compile.sh
@@ -1,3 +1,8 @@
+echo '=== UPDATING THE PROJECT FROM GITHUB REPOSITORY ===';
 git pull;
+echo '';
+
+echo '=== COMPILING CSS AND PREPARING IMAGES ===';
 make bootstrap-css;
 make bootstrap-img;
+echo '';
